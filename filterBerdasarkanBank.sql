5d8b780f640b1cd7224f3007632bdc8d848cf040
projectlutfi0 <147582840+projectlutfi0@users.noreply.github.com>
2024-06-04 17:34:11 +0700

    tugas excercise

--- filterBerdasarkanBank.sql
+++ filterBerdasarkanBank.sql
@@ -1,7 +1,6 @@
-select sum(amount) as "transaction may", bank.name
-	from transaction 
-	inner join bank 
-	on transaction.bank_id = bank.bank_code
-	where transaction_date between '2024-05-01' and '2024-05-31'
-	and bank_id = '1'
-group by bank.name 
+SELECT SUM(amount) as "transaction", bank.name
+FROM transaction 
+INNER JOIN bank ON transaction.bank_id = bank.bank_code
+WHERE transaction_date BETWEEN '2024-05-01' AND '2024-05-31'
+AND bank_id = '1'
+GROUP BY bank.name;
--- filterBerdasarkanTransaction.sql
+++ filterBerdasarkanTransaction.sql
@@ -1,3 +1,3 @@
-select sum(amount) as "transaction may"
+select sum(amount) as "transaction may filter by transact"
 	from transaction 
-	where transaction_date between '2024-05-01' and '2024-05-31'
+	where transaction_date between '2024-05-01' and '2024-05-31'
